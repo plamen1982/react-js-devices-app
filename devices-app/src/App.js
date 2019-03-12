f4a2bbb1fd51879428f955431ab1731c979eb126
plamen1982 <plamen.hristov@ymail.com>
2019-03-12 22:19:38 +0200

    added basic routes without leading to components

--- devices-app/src/App.js
+++ devices-app/src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
-import Navigation from '../src/components/Navigation/Navigation';
+import React, { Component, Fragment } from "react";
+import NavBar from "../src/components/NavBar/NavBar";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 class App extends Component {
     render() {
         return (
-          <Navigation />
+            <Fragment>
+                <NavBar />
+                <Switch>
+                  <Route path="/"/>
+                  <Route path="/all-devices"/>
+                  <Route path="/my-devices"/>
+                  <Route path="/login" />
+                  <Route path="/signup" />
+                </Switch>
+            </Fragment>
         );
     }
 }
---
+++ devices-app/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="navbar-menu">
+                    <Link to="/" className="active" aria-current="page">Home</Link>
+                    <Link to="/all-devices">All Devices</Link>
+                    <Link to="/my-devices">My Devices</Link>
+                    {/* <Link to="/logout">Logout</Link> */}
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                </nav>
+            </header>
+        );
+    }
+}
+
+export default Navigation;

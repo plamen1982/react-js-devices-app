36689566801c8f2e17bb8dade1c5d1ead84d4924
plamen1982 <plamen.hristov@ymail.com>
2019-03-13 21:09:10 +0200

    added static home component and connected with the route

--- devices-app/src/App.js
+++ devices-app/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component, Fragment } from "react";
 import NavBar from "../src/components/NavBar/NavBar";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "../src/components/Home/Home";
 
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <NavBar />
-                <Switch>
-                  <Route path="/"/>
-                  <Route path="/all-devices"/>
-                  <Route path="/my-devices"/>
-                  <Route path="/login" />
-                  <Route path="/signup" />
-                </Switch>
-            </Fragment>
+            <Router>
+                <Fragment>
+                  <NavBar />
+                  <Switch>
+                      <Route path="/" component={Home} exact={true} />
+                      <Route path="/all-devices" exact={true} />
+                      <Route path="/my-devices" exact={true} />
+                      <Route path="/login" exact={true} />
+                      <Route path="/signup" exact={true} />
+                  </Switch>
+                </Fragment>
+            </Router>
         );
     }
 }
---
+++ devices-app/src/components/Home/Home.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+    return (
+        <main>
+            <div className="welcome-wrapper">
+                <div className="welcome">
+                    <h1>Welcome to our book store, tanya !</h1>
+                    <p>
+                        <Link to="/borrow">View your Devices</Link>
+                    </p>
+                </div>
+                <h2>Top Rated</h2>
+                <div className="row">
+                    <div className="card-deck space-top">
+                        <div className="card col-4">
+                            <img
+                                className="card-img-top card-image"
+                                src="https://images-na.ssl-images-amazon.com/images/I/419M8dJUa-L._SY346_.jpg"
+                                alt="What the Wind Knows"
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    What the Wind Knows{" "}
+                                </h5>
+                                <p className="card-text">
+                                    Anne Gallagher grew up enchanted by her
+                                    grandfather’s stories of Ireland.
+                                    Heartbroken at his death, she travels to his
+                                    childhood home to spread his ashes.
+                                </p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted" />
+                                <Link
+                                    to="/details/deviceId"
+                                    type="button"
+                                    className="btn btn-primary float-right btn-sm"
+                                >
+                                    Details
+                                </Link>
+                                <button
+                                    type="button"
+                                    className="btn btn-warning float-right btn-sm"
+                                >
+                                    Order
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Home;

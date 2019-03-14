f4cb1e36737f466d702ee10d97411655d6b76db6
plamen1982 <plamen.hristov@ymail.com>
2019-03-14 14:50:49 +0200

    added DeviceCard to Home view

--- devices-app/src/views/Home/Home.jsx
+++ devices-app/src/views/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainMessage from "../../components/MainMessage/MainMessage";
+import DeviceCard from "../../components/DeviceCard/DeviceCard";
 
 const Home = () => {
     return (
@@ -14,40 +15,12 @@ const Home = () => {
                 <h2>Top Rated</h2>
                 <div className="row">
                     <div className="card-deck space-top">
-                        <div className="card col-4">
-                            <img
-                                className="card-img-top card-image"
-                                src="https://images-na.ssl-images-amazon.com/images/I/419M8dJUa-L._SY346_.jpg"
-                                alt="What the Wind Knows"
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    What the Wind Knows{" "}
-                                </h5>
-                                <p className="card-text">
-                                    Anne Gallagher grew up enchanted by her
-                                    grandfather’s stories of Ireland.
-                                    Heartbroken at his death, she travels to his
-                                    childhood home to spread his ashes.
-                                </p>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted" />
-                                <Link
-                                    to="/details/deviceId"
-                                    type="button"
-                                    className="btn btn-primary float-right btn-sm"
-                                >
-                                    Details
-                                </Link>
-                                <button
-                                    type="button"
-                                    className="btn btn-warning float-right btn-sm"
-                                >
-                                    Order
-                                </button>
-                            </div>
-                        </div>
+                        <DeviceCard />
+                        <DeviceCard />
+                        <DeviceCard />
+                        <DeviceCard />
+                        <DeviceCard />
+                        <DeviceCard />
                     </div>
                 </div>
             </div>

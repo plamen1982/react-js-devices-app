2544660eeadac8d5a126732e876eac8f93679043
plamen1982 <plamen.hristov@ymail.com>
2019-03-14 16:34:06 +0200

    added get method from not yet created crud.js util from the data layer

--- devices-app/src/services/devices-service.js
+++ devices-app/src/services/devices-service.js
@@ -1,9 +1,21 @@
+import { get } from "../data/crud";
+
 class DevicesService {
     constructor() {
         //usualy in production baseUrl is set to ENV variable, because for the different enviroument(QA, QAI and so on) this baseUrl is going to be different
-        // - TODO set in ENV -> something like this -> window.URLS.BOOK_BASE_URL
+        // - TODO set in ENV -> something like this -> window.URLS.DEVICE_BASE_URL
         this.baseUrl = 'http://localhost:5000/device';
+        this.allDevicesUrl = `${this.baseUrl}/all`;
     }
-    getTopRatedDevices() {}
-    getAllDevices() {}
+
+    getTopRatedDevices() {
+
+    }
+
+    getAllDevices() {
+        return get(this.allDevicesUrl);
+    }
+
 }
+
+export default DevicesService;

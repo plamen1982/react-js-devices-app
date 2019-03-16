3a9ea70727e8ac61ae1b4ddb33906f6d61059ac9
plamen1982 <plamen.hristov@ymail.com>
2019-03-17 01:26:58 +0200

    refactoring createDevice form

--- devices-app/src/components/FormDevice/FormDevice.jsx
+++ devices-app/src/components/FormDevice/FormDevice.jsx
@@ -1,14 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class FormDevice extends Component {
-
-    render() {
-        const {model, description, image, creator, price} = this.state;
-        const { titleForm } = this.props;
+const FormDevice = ({ titleForm, buttonName, handleSumbit, handleChange}) => {
         return (
             <div className="form-wrapper">
                 <h1>{titleForm}</h1>
-                <form onSubmit={this.handleOnSumbit}>
+                <form onSubmit={handleSumbit} onChange={handleChange}>
                     <div className="form-group">
                         <label htmlFor="model">model</label>
                         <input
@@ -16,8 +12,6 @@ class FormDevice extends Component {
                             name="model"
                             id="model"
                             placeholder="Enter Device model"
-                            value={model}
-                            onChange={this.handleOnChange}
                         />
                     </div>
                     <div className="form-group">
@@ -27,8 +21,6 @@ class FormDevice extends Component {
                             name="description"
                             id="description"
                             placeholder="Enter Device description"
-                            value={description}
-                            onChange={this.handleOnChange}
                         />
                     </div>
                     <div className="form-group">
@@ -38,8 +30,6 @@ class FormDevice extends Component {
                             name="image"
                             id="image"
                             placeholder="Enter Device image URL"
-                            value={image}
-                            onChange={this.handleOnChange}
                         />
                     </div>
                     <div className="form-group">
@@ -48,8 +38,6 @@ class FormDevice extends Component {
                             type="text"
                             name="creator"
                             id="creator"
-                            value={creator}
-                            onChange={this.handleOnChange}
                         />
                     </div>
                     <div className="form-group">
@@ -59,15 +47,13 @@ class FormDevice extends Component {
                             name="price"
                             id="price"
                             placeholder="Enter Device price"
-                            value={price}
-                            onChange={this.handleOnChange}
                         />
                     </div>
-                    <input type="submit" value="Edit" />
+                    <input type="submit" value={buttonName} />
                 </form>
             </div>
         );
     }
-}
+
 
 export default FormDevice;
--- devices-app/src/views/CreateDevice/CreateDevice.jsx
+++ devices-app/src/views/CreateDevice/CreateDevice.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import withCustomForm from "../../hocs/with-data-from-service";
+import FormDevice, {} from "../../components/FormDevice/FormDevice";
+import withDataFromDeviceService from "../../hocs/with-data-from-device-service";
+
+
 const CreateDevice = (props) => {
     const handleChange = ({ target }) => {
         const inputName = target.name;
@@ -12,58 +15,15 @@ const CreateDevice = (props) => {
         e.preventDefault();
         props.onSubmit();
     };
+
     return (
-        <div className="form-wrapper">
-            <h1>Create New Device</h1>
-            <form onSubmit={handleSumbit} onChange={handleChange}>
-                <div className="form-group">
-                    <label htmlFor="model">model</label>
-                    <input
-                        type="text"
-                        name="model"
-                        id="model"
-                        placeholder="Enter Device model"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <input
-                        type="text"
-                        name="description"
-                        id="description"
-                        placeholder="Enter Device description"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="image">Image URL</label>
-                    <input
-                        type="text"
-                        name="image"
-                        id="image"
-                        placeholder="Enter Device image URL"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="creator">Creator</label>
-                    <input
-                        type="text"
-                        name="creator"
-                        id="creator"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="price">Price</label>
-                    <input
-                        type="number"
-                        name="price"
-                        id="price"
-                        placeholder="Enter Device price"
-                    />
-                </div>
-                <input type="submit" value="Create" />
-            </form>
-        </div>
+        <FormDevice 
+            handleChange={handleChange}
+            handleSumbit={handleSumbit}
+            titleForm="Create New Device"
+            buttonName="Create"
+        />
     );
 }
 
-export default withCustomForm('createDevice')(CreateDevice);
+export default withDataFromDeviceService('createDevice')(CreateDevice);
